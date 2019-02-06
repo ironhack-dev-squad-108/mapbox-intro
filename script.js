925f638bae6d4e361ae7777bcef4f2e602748eb4
Maxence Bouret <maxence.bouret@gmail.com>
2019-02-06 15:16:43 +0100

    Update

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoibWMxMDBzIiwiYSI6ImNqb2E2ZTF3ODBxa3czd2xldHp1Z2FxbGYifQ.U4oatm5RsTXXHQLz5w66dQ';
 var map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11'
+  style: 'mapbox://styles/mapbox/streets-v11',
+  zoom: 8,
+  center: [2.3354096,48.8744289]
 });
 
 // Add a marker
@@ -12,3 +14,17 @@ var marker = new mapboxgl.Marker({
 })
   .setLngLat([13.411026,52.5219216])
   .addTo(map);
+
+// If we can use navigator.geolocation in this browser
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(position => {
+    let lng = position.coords.longitude
+    let lat = position.coords.latitude
+    map.setCenter([lng,lat])
+    let userMarker = new mapboxgl.Marker({
+      color: 'green'
+    })
+      .setLngLat([lng,lat])
+      .addTo(map)
+  })
+}
